5948bad0cc4f15e94b5c63e697320df57e643190
André Uchôa <andreuchoa2@gmail.com>
2022-11-19 21:09:06 -0300

    ajustado

---
+++ script.js
@@ -0,0 +1,60 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+console.log(inputAdultos,inputCriancas,inputDuracao);
+
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+
+    console.log("Calculando")
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value; 
+    let duracao = inputDuracao.value;
+
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) * 0.5 * criancas);
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) * 0.5 * criancas);
+
+    resultado.innerHTML = `<p>${qtdTotalCarne} g de Carne</p>`
+    resultado.innerHTML += `<p>${qtdTotalCerveja} ml de Cerveja</p>`
+    resultado.innerHTML += `<p>${qtdTotalBebidas} ml de Bebidas</p>`
+
+
+    
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6){
+        return 650;
+     
+    } else{
+        return 400;
+
+    }
+    
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6){
+        return 2000;
+     
+    } else{
+        return 1200;
+        
+    }
+}
+    
+
+    function bebidasPP(duracao) {
+        if (duracao >= 6){
+            return 1500;
+         
+        } else{
+            return 1000;
+            
+        }
+}
